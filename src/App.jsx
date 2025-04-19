8da2ffafc002f67668d2c2f3c4b312f59640cd51
anudeep096 <aletianudeepreddy8@gmail.com>
2025-04-20 00:56:26 +0530

    complete banner

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Products from './components/products/Products';
 import AOS from "aos";
 import 'aos/dist/aos.css';
 import TopProducts from './components/TopProducts/TopProducts';
+import Banner from './components/Banner/Banner'
 
 const App = () => {
   React.useEffect(()=>{
@@ -22,6 +23,7 @@ const App = () => {
       <Hero/>
       <Products/>
       <TopProducts/>
+      <Banner/>
     </div>
   )
 }
